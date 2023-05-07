f832517a53397443b5daa941b2f4d02f7996a226
alirdurak <durakaliriza17@gmail.com>
2023-05-07 14:09:32 +0300

    filter buttons style bug fix

--- components/filter-button/index.js
+++ components/filter-button/index.js
@@ -9,10 +9,7 @@ export default function FilterButton() {
     dispatch(filterEventsType(type));
     setFilter(type);
   };
-  const handleClear = () => {
-    dispatch(clearFilter());
-    setFilter();
-  };
+
   return (
     <Flex
       direction="row"
@@ -24,19 +21,16 @@ export default function FilterButton() {
     >
       <Button
         textShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
-        fontWeight="400"
+        fontWeight={filter ? "400" : "700"}
+        color={filter ? "main.1" : "main.2"}
+        borderBottom={!filter && "solid 3px"}
+        borderColor="main.2"
         fontSize="16px"
         variant="unstyled"
         textAlign="center"
         paddingBottom={{ base: "16px", md: "22px" }}
         transition="all ease-in-out 0.3s"
         borderRadius="0px"
-        _focus={{
-          fontWeight: "700",
-          color: "main.2",
-          borderBottom: "solid 3px ",
-          borderColor: "main.2",
-        }}
         _hover={{
           color: "main.2",
         }}
@@ -46,19 +40,16 @@ export default function FilterButton() {
       </Button>
       <Button
         textShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
-        fontWeight="400"
+        fontWeight={filter === "tiyatro" ? "700" : "400"}
+        color={filter === "tiyatro" ? "main.2" : "main.1"}
+        borderBottom={filter === "tiyatro" && "solid 3px"}
+        borderColor="main.2"
         fontSize="16px"
         variant="unstyled"
         textAlign="center"
         paddingBottom={{ base: "16px", md: "22px" }}
         transition="all ease-in-out 0.3s"
         borderRadius="0px"
-        _focus={{
-          fontWeight: "700",
-          color: "main.2",
-          borderBottom: "solid 3px ",
-          borderColor: "main.2",
-        }}
         _hover={{
           color: "main.2",
         }}
@@ -68,19 +59,16 @@ export default function FilterButton() {
       </Button>
       <Button
         textShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
-        fontWeight="400"
+        fontWeight={filter === "konser" ? "700" : "400"}
+        color={filter === "konser" ? "main.2" : "main.1"}
+        borderBottom={filter === "konser" && "solid 3px"}
+        borderColor="main.2"
         fontSize="16px"
         variant="unstyled"
         textAlign="center"
         paddingBottom={{ base: "16px", md: "22px" }}
         transition="all ease-in-out 0.3s"
         borderRadius="0px"
-        _focus={{
-          fontWeight: "700",
-          color: "main.2",
-          borderBottom: "solid 3px ",
-          borderColor: "main.2",
-        }}
         _hover={{
           color: "main.2",
         }}
@@ -90,19 +78,16 @@ export default function FilterButton() {
       </Button>
       <Button
         textShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
-        fontWeight="400"
+        fontWeight={filter === "stand up" ? "700" : "400"}
+        color={filter === "stand up" ? "main.2" : "main.1"}
+        borderBottom={filter === "stand up" && "solid 3px"}
+        borderColor="main.2"
         fontSize="16px"
         variant="unstyled"
         textAlign="center"
         paddingBottom={{ base: "16px", md: "22px" }}
         transition="all ease-in-out 0.3s"
         borderRadius="0px"
-        _focus={{
-          fontWeight: "700",
-          color: "main.2",
-          borderBottom: "solid 3px ",
-          borderColor: "main.2",
-        }}
         _hover={{
           color: "main.2",
         }}
@@ -112,19 +97,16 @@ export default function FilterButton() {
       </Button>
       <Button
         textShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
-        fontWeight="400"
+        fontWeight={filter === "sinema" ? "700" : "400"}
+        color={filter === "sinema" ? "main.2" : "main.1"}
+        borderBottom={filter === "sinema" && "solid 3px"}
+        borderColor="main.2"
         fontSize="16px"
         variant="unstyled"
         textAlign="center"
         paddingBottom={{ base: "16px", md: "22px" }}
         transition="all ease-in-out 0.3s"
         borderRadius="0px"
-        _focus={{
-          fontWeight: "700",
-          color: "main.2",
-          borderBottom: "solid 3px ",
-          borderColor: "main.2",
-        }}
         _hover={{
           color: "main.2",
         }}
@@ -134,19 +116,16 @@ export default function FilterButton() {
       </Button>
       <Button
         textShadow="0px 4px 4px rgba(0, 0, 0, 0.25);"
-        fontWeight="400"
+        fontWeight={filter === "çocuk" ? "700" : "400"}
+        color={filter === "çocuk" ? "main.2" : "main.1"}
+        borderBottom={filter === "çocuk" && "solid 3px"}
+        borderColor="main.2"
         fontSize="16px"
         variant="unstyled"
         textAlign="center"
         paddingBottom={{ base: "16px", md: "22px" }}
         transition="all ease-in-out 0.3s"
         borderRadius="0px"
-        _focus={{
-          fontWeight: "700",
-          color: "main.2",
-          borderBottom: "solid 3px ",
-          borderColor: "main.2",
-        }}
         _hover={{
           color: "main.2",
         }}
